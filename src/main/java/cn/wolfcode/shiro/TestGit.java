49f4c467927d5ae423936da8f20a8bfb6bf8285b
liluoluo2019 <54223829+liluoluo2019@users.noreply.github.com>
2019-08-21 14:34:27 +0800

    Update TestGit.java
    测试从远程库中更新，然后看下本地库是不是会同步更新

--- src/main/java/cn/wolfcode/shiro/TestGit.java
+++ src/main/java/cn/wolfcode/shiro/TestGit.java
@@ -3,5 +3,6 @@ package cn.wolfcode.shiro;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("Test Git");
+        System.out.println("test GitHub);
     }
 }
